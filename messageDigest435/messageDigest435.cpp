3e88437f6bf909640b6b697d55edf549f4d02988
AlikanakelaKarwowski <akk42@zips.uakron.edu>
2020-02-10 16:36:54 -0500

    DONE cleanup time

--- messageDigest435/messageDigest435.cpp
+++ messageDigest435/messageDigest435.cpp
@@ -1,5 +1,5 @@
 /***
-   prepared for CS435 Project 1 part 2
+prepared for CS435 Project 1 part 2
 **/
 
 #include <string.h>
@@ -10,12 +10,11 @@
 
 
 int main(int argc, char *argv[])
-{
-   if ((argc != 4 && argc != 3) || (argv[1][0]!='s' && argv[1][0]!='v'))
+  {
+    if ((argc != 4 && argc != 3) || (argv[1][0]!='s' && argv[1][0]!='v'))
       std::cout << "wrong format! should be \"a.exe s filename\"\n";
-   else {
+    else {
       std::string filename = argv[2];
-
 
       //read the file
       std::streampos begin,end;
@@ -36,37 +35,87 @@ int main(int argc, char *argv[])
       myfile2.write (memblock, size); //write to a file
       myfile2.close();
 
-      std::cout<<memblock;
+      //std::cout<<memblock;
 
-      if (argv[1][0]=='s') {
-         std::cout << "\n"<<"Need to sign the doc.\n";
-         //make hash function
+      if (argv[1][0]=='s')
+      {
+        std::cout << "\n"<<"Need to sign the doc.\n";
 
-         std::string sha256Hash = sha256(memblock);
-         BigUnsignedInABase shaBase16 = BigUnsignedInABase(sha256Hash, 16);
-         BigUnsigned shaBase10 = shaBase16;
+        //make hash function
+        std::string sha256Hash = sha256(memblock);
+        //convert from from base 16 to base 10
+        BigUnsignedInABase shaBase16 = BigUnsignedInABase(sha256Hash, 16);
+        //save as originalMessage
+        BigUnsigned originalMessage = shaBase16;
 
-         //open up the file generated in
-         std::fstream d_n("..\\messageDigest435\\d_n.txt", std::fstream::in | std::fstream::out);
-         std::string temp;
-         d_n >> temp;
-         BigUnsigned bigD = stringToBigUnsigned(temp);
-         d_n >>temp;
-         BigUnsigned bigN = stringToBigUnsigned(temp);
+        //open up the d_n.txt
+        std::fstream d_n("d_n.txt", std::fstream::in | std::fstream::out);
+        std::string temp;
+        //read the first line into the temp string
+        d_n >> temp;
+        //convert from string to big unsigned
+        BigUnsigned bigD = stringToBigUnsigned(temp);
+        temp = "";
+        //read second line into temp string
+        d_n >> temp;
+        //convert from string to big unsigned
+        BigUnsigned bigN = stringToBigUnsigned(temp);
+        //do modexp to get the signature
+        BigUnsigned signature = modexp(originalMessage,bigD,bigN);
 
-         BigUnsigned signature = modexp(shaBase10,bigD,bigN);
-         std::ofstream signed;
-         signed.open("file.txt.signature");
-         signed << signature;
-         signed.close();
-         std::cout << "Message is signed.";
+        //create file signature
+        std::ofstream sign;
+        sign.open( filename + ".signature");
+        sign << signature;
+        sign.close();
+        std::cout << "Message is signed.\n";
       }
       else {
-         std::cout << "\n"<<"Need to verify the doc.\n";
-         //.....
+        std::cout << "\n"<<"Need to verify the doc.\n";
 
+        //make hash function
+        std::string sha256Hash = sha256(memblock);
+        //convert from from base 16 to base 10
+        BigUnsignedInABase shaBase16 = BigUnsignedInABase(sha256Hash, 16);
+        //save as originalMessage
+        BigUnsigned originalMessage = shaBase16;
+
+        //open up the e_n.txt file
+        std::fstream e_n("e_n.txt", std::fstream::in | std::fstream::out);
+        std::string temp;
+        //read the first line into the temp string
+        e_n >> temp;
+        //convert from string to big unsigned
+        BigUnsigned bigE = stringToBigUnsigned(temp);
+        temp = "";
+        //read second line into temp string
+        e_n >> temp;
+        //convert from string to big unsigned
+        BigUnsigned bigN = stringToBigUnsigned(temp);
+        e_n.close();
+
+        //open signature file
+        std::fstream sign;
+        std::string signatureKey;
+        sign.open(argv[3]);
+        //read the signature from file
+        sign >> signatureKey;
+        sign.close();
+
+        //convert string to BigUnsigned
+        BigUnsigned signature = stringToBigUnsigned(signatureKey);
+
+        BigUnsigned decrypt = modexp(signature,bigE,bigN);
+        if(originalMessage == decrypt)
+        {
+          std::cout << "Document is Authentic.\n";
+        }
+        else
+        {
+          std::cout << "Document is not Authentic.\n";
+        }
       }
       delete[] memblock;
     }
-    return 0;
-}
+  return 0;
+  }
