11059b949a14398dc9b0bb0ae6e888a2869e17c3
Alex K <alexkarrrr@gmail.com>
2020-03-04 17:36:42 -0500

    create error checking

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+
+
 int main(int argc, char* argv[])
 {
+    std::cout << "Number of arguments=" << argc << std::endl;
+    if (argc !=3)
+        std::cout << "Incorrect number of arguments.\nFormat should be \"cHull.exe G test.txt\"" << std::endl;
+    if (*argv[2] == 'G')
+    {
+
+    }
 }
