029122f25dd0595cb876f1d718bc22b7021d8bfc
Alex K <alexkarrrr@gmail.com>
2020-03-04 22:27:56 -0500

    Create Jarvis March functions and classes

---
+++ Project2/JarvisMarch.hpp
@@ -0,0 +1,19 @@
+
+struct Point
+{
+    int x, y;
+};
+
+class Jarvis
+{
+    public:
+        //find orientation of ordered triplet
+        int orientation(Point p, Point q, Point r);
+
+        //prints convex hull of a set of n points
+        void convexHull(Point points[], int n);
+
+        //point structure
+
+
+};
